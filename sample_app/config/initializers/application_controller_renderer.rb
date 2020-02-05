06cc4e1359d477305f3b139e6ee6d51f1e492652
Riu <y1a1r2y5u@gmail.com>
2020-02-05 20:10:11 +0900

    [Start]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '587ba1bf50cd985c45854e57638ff24f11b60eb7e4671bfbf7f6d703937853eec282aa1facac33d3e226231440ce99ee9fb2a0e91a9a35335bbed6a5d13ef90a'
