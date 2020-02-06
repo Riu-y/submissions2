1ef45df81fed5316059300966c54ecbc33565790
Riu <y1a1r2y5u@gmail.com>
2020-02-06 12:24:06 +0900

    [Start]4-2

--- CARAVAN/config/routes.rb
+++ CARAVAN/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'blogs/show'
   get 'blogs/new'
   get 'blogs/edit'
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
